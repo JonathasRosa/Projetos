010005cbbcf00ab84d97a5e99ea5abcc21a418c6
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-12-31 11:11:07 -0300

    Update Pokedex-2 with Angular

--- Pokedex-2/src/app/app.module.ts
+++ Pokedex-2/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 //Modules
 import { PagesModule } from './pages/pages.module';
+
 //Componentes
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
