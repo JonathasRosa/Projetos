9b52672991c6aebb00e6fb83751acc3d41adae6c
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-01-03 16:43:11 -0300

    Update Pokedex with Angular

--- Pokedex-2/src/app/service/poke-api.service.ts
+++ Pokedex-2/src/app/service/poke-api.service.ts
@@ -1,25 +1,41 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//Observable
 import { Observable } from 'rxjs';
-import { tap } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
+  apiGetPokemons(url: any) {
+    throw new Error('Method not implemented.');
+  }
 
   private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=50';
 
   constructor(
     private http: HttpClient
   ) { }
-
+    //status do pokemon
   get apiListAllPokemons():Observable<any>{
     return this.http.get<any>(this.url).pipe(
-      tap(res => res),
-      tap(res => {
-        console.log(res)
+      tap( res => res ),
+      tap( res => {
+        res.results.map((resPokemons: any) => {
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          );
+        })
       })
+    )
+  }
+  //Details do pokemon
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(
+      map(
+        res => res
+      )
     )
   }
 }
