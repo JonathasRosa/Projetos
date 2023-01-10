ee124aafc62e5765b35217a6e9882b5e5a452480
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-01-10 16:47:33 -0300

    Update Pokedex-2 with Angular

--- Pokedex-2/src/app/service/poke-api.service.ts
+++ Pokedex-2/src/app/service/poke-api.service.ts
@@ -8,18 +8,18 @@ import { map, tap } from "rxjs/operators";
   providedIn: 'root'
 })
 export class PokeApiService {
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=50';
-
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
   constructor(
     private http: HttpClient
   ) { }
-
   get apiListAllPokemons():Observable<any>{
     return this.http.get<any>(this.url).pipe(
       tap( res => res ),
       tap( res => {
         res.results.map((resPokemons: any) => {
-          this.apiGetPokemons(resPokemons.url).subscribe((res: any) => resPokemons.status = res);
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          );
         })
       })
     )
--- Pokedex-2/src/main.ts
+++ Pokedex-2/src/main.ts
@@ -1,7 +1,13 @@
+import { environment } from './../../ToDo-List/src/environments/environment.prod';
+
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
